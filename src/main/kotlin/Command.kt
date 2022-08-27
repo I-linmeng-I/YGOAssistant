6eb9b33dc94c671eff77577ef2ca14fdaafe16ed
水纹霖霖 <67990349+CauchyDOOM@users.noreply.github.com>
2022-08-27 20:35:19 +0800

    Add files via upload

--- src/main/kotlin/Command.kt
+++ src/main/kotlin/Command.kt
@@ -377,18 +377,17 @@ class Command {
         }
         val CardData2 = GetWebSourceCode("https://ygocdb.com/card/"+ResultMatch[cardNumber-1].groupValues[1]) 
         val avail = Regex("""<i class="(.*?)">""").find(CardData2)
-        var availMatch = ""
-        if(avail == l0){
-            availMatch = "禁止卡"
+        if(avail = "l0"){
+            val availMatch = "禁止卡"
         }
-        else if(avail == l1){
-            availMatch = "限制卡"
+        else if(avail = "l1"){
+            val availMatch = "限制卡"
         }
-        else if(avail == l2){
-            availMatch = "准限制卡"
+        else if(avail = "l2"){
+            val availMatch = "准限制卡"
         }
         else{
-            availMatch = "无限制卡"
+            val availMatch = "无限制卡"
         }
 
         val CardData = GetWebSourceCode("https://ygocdb.com/api/v0/?search="+ResultMatch[cardNumber-1].groupValues[1])
